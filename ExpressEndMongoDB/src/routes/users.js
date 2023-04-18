5d0475a7a272e5ddaf7c7c358d313327ad90bf87
Анжелика <lika-2020@yandex.ru>
2023-04-18 16:40:29 +0300

    start project

---
+++ ExpressEndMongoDB/src/routes/users.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users");
+
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+
+module.exports = router;
